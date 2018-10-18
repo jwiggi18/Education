10e2a6c61e6c7d4b9f64a954f8dcc6739e0d04e9
Jodie Wiggins <jodiewiggins18@gmail.com>
2018-10-18 11:27:26 -0400

    Code to get a tree of WV state symbols

---
+++ getting_trees.R
@@ -0,0 +1,25 @@
+#How to get phylotastic/datelife to call the correct phylogeny
+
+library(rphylotastic)
+library(datelife)
+#to pull the scientifc names from the list of state symbols in my .txt file
+
+WV_symbols <- file_get_scientific_names(file_name = "state_symbols/West_Virginia.txt")
+
+WV_phy1 <- taxa_get_otol_tree(taxa = WV_symbols)
+
+#WV_phy2 <- taxa_get_phylomatic_tree(taxa = WV_symbols)
+
+source_tree <- c("Open Tree of Life")
+
+library(ape)
+#par = set or query graphical parameters
+#xpd = plotting parameter
+par(xpd = FALSE, mfrow = c(1, length(WV_phy1)), mai = c(0.1, 0.1, 0.5, 0.1), oma = c(0, 1, 2, 1))
+for (i in 1:length(WV_phy1)){
+    plot(ladderize(WV_phy1))
+    mtext(paste("from", source_tree[i]), cex = 1, side = 3, line = 1)
+}
+mtext("West Virginia State Symbols", outer = T, side = 3, cex = 1.5, line = 0)
+
+
