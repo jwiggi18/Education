b685d166c0f60dc54c4550e9238b1c8c361ba1ec
Brian O'Meara <omeara.brian@gmail.com>
2018-11-12 23:38:05 -0500

    better plotting

--- sd_zooscrape.R
+++ sd_zooscrape.R
@@ -11,14 +11,14 @@ library(rphylotastic)
 
 get_san_diego_zoo_animals <- function() {
     animals <- read_html("https://animals.sandiegozoo.org/animals")
-    links  <- animals %>% 
+    links  <- animals %>%
         html_nodes("a") %>% html_attr("href") # use rvest to get all the a href elements
     links <- links[grepl("/animals/", links)] # grepl to only get the links to the relevant pages. This is the sort of manual processing that is annoying but often necessary.
-    
+
     links <- unique(links) # get rid of duplicates (b/c looking at the links, sometimes had the same link
-    
+
     all.taxa <- c() # a vector to store the names
-    
+
     for (i in seq_along(links)) { # now loop over the links
         print(paste("doing link", i, "of", length(links), links[i]))
         taxa <- NULL # store NULL first, so we can detect failure
@@ -39,14 +39,14 @@ library(taxize)
 
 #find accepted names
 #data_source_ids, 179 = Open Tree of Life reference taxonomy
-gnr_taxa <- taxize::gnr_resolve(names = taxa, data_source_ids = 179, best_match_only = TRUE, canonical = TRUE) 
+gnr_taxa <- taxize::gnr_resolve(names = taxa, data_source_ids = 179, best_match_only = TRUE, canonical = TRUE)
 #View output
 View(gnr_taxa)
 
-gnr_vector <- gnr_taxa$matched_name2 
+gnr_vector <- gnr_taxa$matched_name2
 
 
-# Luna's work around       
+# Luna's work around
 sd_phy <- get_otol_synthetic_tree(gnr_vector)
 
 source_tree <- c("Open Tree of Life")
@@ -54,7 +54,18 @@ source_tree <- c("Open Tree of Life")
 library(ape)
 #par = set or query graphical parameters
 #xpd = plotting parameter
-par(xpd = FALSE, mfrow = c(1,1), mai = c(0.1, 0.1, 0.5, 0.1), oma = c(0, 1, 2, 1))
-plot(ladderize(sd_phy))
-mtext(paste("from", source_tree[i]), cex = 1, side = 3, line = 1)
-mtext("San Diego Zoo Animals", outer = T, side = 3, cex = 1.5, line = 0)
+#par(xpd = FALSE, mfrow = c(1,1), mai = c(0.1, 0.1, 0.5, 0.1), oma = c(0, 1, 2, 1))
+#plot(ladderize(sd_phy))
+#mtext(paste("from", source_tree[i]), cex = 1, side = 3, line = 1)
+#mtext("San Diego Zoo Animals", outer = T, side = 3, cex = 1.5, line = 0)
+plot(sd_phy, cex=0.2, main="San Diego Zoo Animals")
+ape::axisPhylo()
+
+
+pdf(file="SanDiegoZoo.pdf", width=5, height=10)
+sd_phy$node.label[grepl("mrcaott", sd_phy$node.label)] <- ""
+plot(sd_phy, cex=0.2, main="San Diego Zoo Animals")
+nodelabels(sd_phy$node.label, frame="none", cex=0.2, col="red")
+ape::axisPhylo()
+dev.off()
+system("open SanDiegoZoo.pdf")
