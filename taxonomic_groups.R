42d789e4271c1e4d0e01eb145f67c5e2dc5595ea
Jodie Wiggins <jodiewiggins18@gmail.com>
2019-03-14 14:45:18 -0400

    trees based on taxonomic groups

---
+++ taxonomic_groups.R
@@ -0,0 +1,31 @@
+library(shiny)
+library(datelife)
+library(rphylotastic)
+
+
+mams <- c("Hippopotamus amphibius", "Trichechus manatus", "Tursiops truncatus", "Equus caballus", "Canis lupus", "Pan troglodytes", "Panthera leo")
+mams_tree <- rphylotastic::taxa_get_otol_tree(taxa = mams)
+ape::plot.phylo(mams_tree)
+
+
+
+groups_list <- c("Mammals", "Dinosaurs", "Amphibians", "Echinoderms", "Mollusks")
+
+ui <- fluidPage(theme = shinythemes::shinytheme("journal"),
+    titlePanel("Phylogenies as Models"),
+    br(),
+    br(),
+
+    fluidRow(
+      column(4, align="center",
+        SelectInput("group", "Choose a group:"
+          choices = groups_list),
+          multiple = FALSE)
+    ),
+
+    fluidRow(plotOutput("tree"))
+)
+
+server <- function(input, output) {
+  output$tree <- reactive({input$group})
+}
