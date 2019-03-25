5d689b3b1bee020d509c230b4c4c0458add7bb10
Jodie Wiggins <jodiewiggins18@gmail.com>
2019-03-25 12:41:13 -0400

    if else throwing error

--- taxonomic_groups.R
+++ taxonomic_groups.R
@@ -1,48 +1,58 @@
 library(shiny)
 library(rphylotastic)
+library(shinythemes)
+library(ape)
 
-
-Mammals <- c("Hippopotamus amphibius", "Trichechus manatus", "Tursiops truncatus", "Equus caballus", "Canis lupus", "Pan troglodytes", "Panthera leo")
-#mams_tree <- rphylotastic::taxa_get_otol_tree(taxa = mams)
-
-Amphibians <- c("Caecilia thompsoni", "Dendrobates leucomelas", "Ambystoma tigrinum")
-
+# function to get tree
 getTree <- function(taxa) {
   rphylotastic::taxa_get_otol_tree(taxa)
 }
 
+Mammals <- c("Hippopotamus amphibius", "Trichechus manatus", "Tursiops truncatus", "Equus caballus", "Canis lupus", "Pan troglodytes", "Panthera leo")
+mams_tree <- getTree(Mammals)
 
-groups_list <- c("Mammals", "Dinosaurs", "Amphibians", "Echinoderms", "Mollusks")
+Amphibians <- c("Caecilia thompsoni", "Rhinella marina", "Hyloxalus abditaurantius", "Rhacophorus nigropalmatus", "Ambystoma tigrinum")
+amph_tree <- getTree(Amphibians)
+
+Echinoderms <- c("Pisaster ochraceus", "Mesocentrotus franciscanus", "Echinarachnius parma", "Cucumaria miniata")
+ech_tree <- getTree(Echinoderms)
+
+Mollusks <- c("Glaucus atlanticus", "Cornu aspersum", "Sepia officinalis", "Octopus vulgaris", "Bithynia tentaculata")
+mol_tree <- getTree(Mollusks)
+
 
 ui <- fluidPage(theme = shinythemes::shinytheme("journal"),
+  #app title
     titlePanel("Phylogenies as Models"),
-    br(),
-    br(),
 
+    #input: Groups students can choose from
     fluidRow(
-      column(4, align="center",
-        selectInput("group", "Choose a group:",
-          choices = groups_list, selected = "Mammals", multiple = FALSE)
+        column(4, align="center",
+            selectInput("group", "Choose a group:",
+              choices = c("Mammals", "Amphibians", "Echinoderms", "Mollusks"),
+              multiple = FALSE)
         )
-      ),
+    ),
 
+      #row to display output
     fluidRow(plotOutput("tree"))
 )
 
 server <- function(input, output) {
-  #output$tree <- renderPlot({ape::plot.phylo(mams_tree)})
-  #output$tree <- renderPlot({
-    #tree <- getTree(taxa = Mammals)
-    #ape::plot.phylo(tree)
-  #})
-  rGroups <- reactive({
-    input$groups
-  })
+
 
   output$tree <- renderPlot({
-    gTree <- (getTree(taxa = rGroups)
-    ape::plot.phylo(gTree))
-  })
 
+      if (input$groups == "Mammals") {ape::plot.phylo(mams_tree)}
+
+      else if (input$groups == "Amphibians") {ape::plot.phylo(amph_tree)}
+
+      else if (input$groups == "Echinoderms") {ape::plot.phylo(ech_tree)}
+
+      else {ape::plot.phylo(mol_tree)}
+
+    })
+
+}
 
 shinyApp(ui = ui, server = server)
